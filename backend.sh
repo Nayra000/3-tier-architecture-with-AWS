3c68243df14afc3f59582273773b23c208884047
nayra000 <youssefnayra26@gmail.com>
2025-05-28 18:34:42 +0300

    modify bash scripts

--- backend.sh
+++ backend.sh
@@ -1,14 +1,12 @@
-#!/bin/sh
-
-sudo yum update -y
+#!/bin/bash
+sudo apt update -y 
 
 sudo apt install apache2 -y
+
+echo 'Hello from backend' | sudo tee /var/www/html/index.html > /dev/null
 
 sudo systemctl start apache2
 
 sudo systemctl enable apache2
 
-
-sudo yum install -y https://repo.mysql.com/mysql80-community-release-el7-3.noarch.rpm
-
-sudo yum install -y mysql-community-client
+sudo apt install mysql-client-core-8.0 
--- web.sh
+++ web.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-sudo apt update -y
+sudo apt update -y 
+
 sudo apt install apache2 -y
-#sudo touch /var/www/html/home.html 
-#echo 'WEBSITE Hosted!!' | sudo tee /var/www/html/home.html
+
+echo 'Hello from frontend' | sudo tee /var/www/html/index.html > /dev/null
+
 sudo systemctl start apache2
+
 sudo systemctl enable apache2
